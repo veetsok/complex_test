df8d5a7de72ee35c8a4f8e3481418a23d95c32a3
Ivan <94634131+veetsok@users.noreply.github.com>
2024-04-13 23:30:45 +0400

    add cardBlock

--- src/user.InterfaceLayer/Libraries/Widgets/CardBlock.widget/index.tsx
+++ src/user.InterfaceLayer/Libraries/Widgets/CardBlock.widget/index.tsx
@@ -2,16 +2,42 @@ import Image from "next/image";
 import React from "react";
 import TextAtom from "../../UI_KIT/Atoms/Text.Atom";
 import TextAtomEnum from "../../UI_KIT/Atoms/Text.Atom/enum";
+import ButtonAtom from "../../UI_KIT/Atoms/Button.Atom";
+import ButtonAtomEnum from "../../UI_KIT/Atoms/Button.Atom/enum";
 
-interface CardBlockProps {}
+interface CardBlockProps {
+  image_url?: string; // Make image_url optional
+  title?: string;
+  description?: string;
+  price?: number;
+}
 
-const CardBlock: React.FC<CardBlockProps> = () => {
+const CardBlock: React.FC<CardBlockProps> = (props) => {
+  const { image_url, title, description, price } = props;
+
   return (
     <div className="grid grid-cols-[minmax(0,auto)] grid-rows-[auto auto auto minmax(0,100%)] py-[9px] px-[10px] bg-bgCard rounded-[15px] flex flex-col h-full">
-      <div className="w-[281px] h-[369px]">{/* <Image /> */}</div>
-      <TextAtom type={TextAtomEnum.enum_h2}>Заголовок</TextAtom>
-      <TextAtom type={TextAtomEnum.enum_h2}>описание</TextAtom>
+      <div className="w-[281px] h-[369px]">
+        {image_url ? (
+          <Image
+            src={image_url}
+            alt={"image"}
+            width={281}
+            height={369}
+            layout="responsive"
+          />
+        ) : (
+          <div className="placeholder-image">Placeholder Image</div>
+        )}
+      </div>
+      <TextAtom type={TextAtomEnum.enum_h2}>{title}</TextAtom>
+      <TextAtom type={TextAtomEnum.enum_h2}>{description}</TextAtom>
+      <TextAtom type={TextAtomEnum.enum_h2} className="text-textWhite">
+        Цена: {price} ₽
+      </TextAtom>
+      <ButtonAtom type={ButtonAtomEnum.enum_defaultButton}>Купить</ButtonAtom>
     </div>
   );
 };
+
 export default CardBlock;
