c2a2147cb49eb44f020b73f9a8391902f9e894fb
Ivan <94634131+veetsok@users.noreply.github.com>
2024-04-14 06:02:47 +0400

    update styles

--- src/user.InterfaceLayer/Libraries/Widgets/Cart.widget/index.tsx
+++ src/user.InterfaceLayer/Libraries/Widgets/Cart.widget/index.tsx
@@ -29,7 +29,7 @@ const CartWidget: React.FC<CartWidgetProps> = () => {
       <TextAtom type={TextAtomEnum.enum_h2}>Добавленные товары</TextAtom>
       {memoizedItems}
       <div className="flex justify-between">
-        <div className="flex items-center bg-black rounded-[15px] py-2 px-4 max-w-[400px]">
+        <div className="flex items-center bg-black gap-5 rounded-[15px] py-2 px-4 max-w-[400px]">
           <TextAtom type={TextAtomEnum.enum_h2} className="text-white">
             +7
           </TextAtom>
