6a33e021fc5494a926ea6f9073f3ecba346f33e1
Ivan <94634131+veetsok@users.noreply.github.com>
2024-04-14 22:12:11 +0400

    add

--- src/user.InterfaceLayer/Libraries/UI_KIT/Organisms/CallNumber.molecule/index.tsx
+++ src/user.InterfaceLayer/Libraries/UI_KIT/Organisms/CallNumber.molecule/index.tsx
@@ -1,11 +1,12 @@
 // CallNumber.tsx
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import ButtonAtom from "../../Atoms/Button.Atom";
 import ButtonAtomEnum from "../../Atoms/Button.Atom/enum";
 import InputAtom from "../../Atoms/Input.Atom";
 import { InputAtomEnum } from "../../Atoms/Input.Atom/enum";
 import useCartStore from "@/business.InterfaceLayer/hooks/store/localstorage/useCartStore";
 import { useOrder } from "@/business.InterfaceLayer/hooks/store/useOrder";
+import ModalWindow from "../../Cells/ModalWindow.cells";
 
 interface CallNumberProps {}
 
@@ -13,59 +14,55 @@ const CallNumber: React.FC<CallNumberProps> = () => {
   const { phoneNumber, setPhoneNumber, items, clearCart } = useCartStore();
   const { submitOrder } = useOrder();
 
+  const [isShowModal, setIsShowModal] = useState(false);
+  const openModal = useCallback(() => {
+    setIsShowModal(true);
+  }, [setIsShowModal]);
+
   const handlePhoneChange = useCallback(
     (value: string) => {
       setPhoneNumber(value);
     },
     [setPhoneNumber]
   );
-
   const handleSubmit = async () => {
-    if (phoneNumber.length !== 12) {
-      // Handle invalid phone number
+    if (phoneNumber.length !== 11) {
       return;
     }
-
     try {
-      // Prepare the cart data
       const cart = items.map(({ id, quantity }) => ({ id, quantity }));
-
-      // Call the submitOrder function from the useOrder hook
       await submitOrder(phoneNumber, cart);
-
-      // If successful, clear the cart or show success message
-      // For simplicity, let's assume the server returns success
       alert("Order submitted successfully!");
-      // Clear the cart
       clearCart();
     } catch (error) {
-      // Handle error
       console.error("Error submitting order:", error);
-      // Show error message
       alert("Failed to submit order. Please try again.");
     }
   };
 
-  console.log(phoneNumber);
+  console.log("79163452487".length);
 
   return (
-    <div className="flex justify-between">
-      <div className="flex items-center bg-black gap-5 rounded-[15px] py-2 px-4 max-w-[400px]">
-        <InputAtom
-          type={InputAtomEnum.TEL}
-          value={phoneNumber}
-          onChange={handlePhoneChange}
-          placeholder="+7 (___) ___-__-__"
-        />
+    <>
+      {isShowModal && <ModalWindow close={setIsShowModal} />}
+      <div className="flex justify-between">
+        <div className="flex items-center bg-black gap-5 rounded-[15px] py-2 px-4 max-w-[400px]">
+          <InputAtom
+            type={InputAtomEnum.TEL}
+            value={phoneNumber}
+            onChange={handlePhoneChange}
+            placeholder="+7 (___) ___-__-__"
+          />
+        </div>
+        <ButtonAtom
+          className="max-w-[268px]"
+          type={ButtonAtomEnum.enum_buyButton}
+          onClick={openModal}
+        >
+          Заказать
+        </ButtonAtom>
       </div>
-      <ButtonAtom
-        className="max-w-[268px]"
-        type={ButtonAtomEnum.enum_buyButton}
-        onClick={handleSubmit}
-      >
-        Заказать
-      </ButtonAtom>
-    </div>
+    </>
   );
 };
 export default React.memo(CallNumber);
