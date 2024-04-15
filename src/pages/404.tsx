ac85e25aba65e08a184105f86ace165b8594b801
Ivan <94634131+veetsok@users.noreply.github.com>
2024-04-15 07:06:53 +0400

    update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { globalContainer } from "@/user.InterfaceLayer/constants/styles/CommonStyles";
 import dynamic from "next/dynamic";
+import TextAtom from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Text.Atom";
+import TextAtomEnum from "@/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Text.Atom/enum";
 
 const Page404: React.FC = () => {
-  return <div className={`${globalContainer}`}>нет страницы </div>;
+  return (
+    <div
+      className={`${globalContainer} flex flex-col gap-4 justify-center items-center h-[95vh]`}
+    >
+      <TextAtom type={TextAtomEnum.enum_h1}>нет страницы</TextAtom>
+    </div>
+  );
 };
 export default React.memo(Page404);
