c5ec66e31d2a337a75b614f09a68caed6781b534
Ivan <94634131+veetsok@users.noreply.github.com>
2024-04-15 08:32:15 +0400

    update input

--- src/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Input.Atom/styles/styled.ts
+++ src/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Input.Atom/styles/styled.ts
@@ -1,4 +1,5 @@
+import mobile from "./mobile.module.css";
+
 export const rangeStyle = "w-full bg-tiffany";
 
-export const inputStyle =
-  "text-h2 h-auto rounded bg-transparent placeholder-white flex w-full cursor-pointer outline-none text-white";
+export const inputStyle = `${mobile.inputSize} text-h2 h-auto rounded bg-transparent placeholder-white flex w-full cursor-pointer outline-none text-white`;
